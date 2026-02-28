aa77e8240518dacb580703894531dd3e808e825a
Harsh Nayak <harshnyk123@gmail.com>
2026-02-28 16:11:08 +0530

    deploying step 5

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,6 +16,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5174",
+  "https://talent-flow-mini-hiring-platform-two.vercel.app/",
 ];
 app.use(
   cors({
